f1ecc604bbc68a4f49803fed7195bf0ef16a2b29
malinka <malinka@vmi.asia>
2018-06-10 06:24:40 +0700

    add method to support hello name

--- src/main/java/malinka/web/service/MessageService.java
+++ src/main/java/malinka/web/service/MessageService.java
@@ -7,9 +7,28 @@ import malinka.web.model.MessageModel;
 @Service
 public class MessageService {
 
+	/**
+	 * 
+	 * @return message model
+	 */
 	public MessageModel getMessageModel() {
 		
 		MessageModel model = new MessageModel("Hello World!");
+		
+		return model;
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @return message model
+	 * @throws IllegalArgumentException
+	 */
+	public MessageModel getMesageModel(String name) throws IllegalArgumentException {
+		
+		if(name == null || name.isEmpty()) throw new IllegalArgumentException("input name is invalid");
+		
+		MessageModel model = new MessageModel(String.format("Hello %s", name));
 		
 		return model;
 	}
--- src/test/java/malinka/web/service/MessageServiceTest.java
+++ src/test/java/malinka/web/service/MessageServiceTest.java
@@ -1,21 +1,72 @@
 package malinka.web.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import malinka.web.model.MessageModel;
 
 public class MessageServiceTest {
 
-	@Test
-	public void test() {
+	MessageService msgService;
+	
+	@Before
+	public void before() {
 		
-		MessageService msgService = new MessageService();
+		msgService = new MessageService();
+		
+	}
+	
+	@Test
+	public void testHelloWorld() {
 		
 		MessageModel model = msgService.getMessageModel();
 		
 		assertEquals("Hello World!", model.getMessage());
+		
+	}
+	
+	@Test
+	public void testThrowIllegalArgumentWhenNameIsNull() {
+		
+		String name = null;
+		
+		try {
+			
+			msgService.getMesageModel(name);
+			
+			fail("expected illegal argument exception, but it never came through");
+			
+		} catch(IllegalArgumentException e) {
+			
+		}
+	}
+	
+	@Test
+	public void testThrowIllegalArgumentExceptionWhenNameisEmpty() {
+		
+		String name = "";
+		
+		try {
+			
+			msgService.getMesageModel(name);
+			
+			fail("expected illegal argument exception, but it never came through");
+			
+		} catch(IllegalArgumentException e) {
+			
+		}
+		
+	}
+	
+	@Test
+	public void testHelloName() {
+		
+		MessageModel model = msgService.getMesageModel("Malinka");
+		
+		assertEquals("Hello Malinka", model.getMessage());
 		
 	}
 
